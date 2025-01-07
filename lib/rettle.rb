805d2ccadefcac228a79b932b0733a3bb392bc1e
longicorn <longicorn.c@gmail.com>
2025-01-07 20:20:43 +0900

    add first version

--- lib/rettle.rb
+++ lib/rettle.rb
@@ -1,8 +1,31 @@
 # frozen_string_literal: true
 
 require_relative "rettle/version"
+require_relative "rettle/task"
 
-module Rettle
-  class Error < StandardError; end
-  # Your code goes here...
+class Rettle
+  attr_accessor :extract, :transforms, :load
+
+  def setup
+    # set pipeline
+    @pipeline = []
+    @pipeline << @extract
+    @pipeline << @transforms
+    @pipeline << @load
+    @pipeline = @pipeline.flatten.compact
+
+    # set next task
+    @pipeline.each_cons(2) do |before, after|
+      before.next_task = after
+    end
+  end
+
+  def run
+    setup
+    @pipeline.each(&:run)
+  end
+
+  def join
+    @pipeline.each(&:join)
+  end
 end
---
+++ lib/rettle/task.rb
@@ -0,0 +1,63 @@
+class Rettle
+  class Task
+    def initialize
+      @r, @w = IO.pipe
+    end
+    attr_accessor :next_task
+
+    # read from file descriptor
+    def read
+      size = @r.readline.chomp.to_i
+      mstr = @r.read(size)
+      Marshal.load(mstr)
+    rescue EOFError
+      # read was closed
+      nil
+    end
+
+    # read from before task
+    def recv
+      read
+    end
+
+    # write to file descriptor
+    def write(data)
+      mdata = Marshal.dump(data)
+      @w.write mdata.size
+      @w.write "\n"
+      @w.flush
+      @w.write mdata
+      @w.flush
+    end
+
+    # write from next task
+    def send(data)
+      next_task.write(data)
+    end
+
+    def close
+      @w.close
+    end
+
+    def finish
+      next_task&.close
+    end
+
+    def process
+      @proc = lambda do
+        yield self
+        finish
+      end
+    end
+
+    def run
+      @thread = Thread.new do
+        @proc.call
+      end
+    end
+
+    def join
+      @thread.join
+    end
+  end
+end
