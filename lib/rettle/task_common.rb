f30224b17bcf97567b962403c33d687064009977
longicorn <longicorn.c@gmail.com>
2025-01-28 20:09:39 +0900

    feat!: add task_common

---
+++ lib/rettle/task_common.rb
@@ -0,0 +1,18 @@
+require 'tempfile'
+
+class Rettle
+  module TaskCommon
+    def encoding!(path, encoding: :utf8)
+      tmppath = "#{path}_#{encoding}"
+      tmp = Tempfile.open(tmppath) do |tf|
+        File.open(path) do |f|
+          f.each_line do |line|
+            tf.write(line.toutf8)
+          end
+        end
+        tf
+      end
+      FileUtils.mv(tmp.path, path)
+    end
+  end
+end
