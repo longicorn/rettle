211edef72b0b9c6389425004c4747c60121002d3
longicorn <longicorn.c@gmail.com>
2025-02-01 00:46:33 +0900

    feat: redesign download

--- lib/rettle/task_extract.rb
+++ lib/rettle/task_extract.rb
@@ -1,42 +1,62 @@
 # frozen_string_literal: true
 
+require 'pathname'
 require 'fileutils'
 require 'net/http'
 
 class Rettle
   module TaskExtract
-    def after_download(file_name)
-      case `file #{file_name}`
+    class UndefinedError < StandardError; end
+
+    def after_download(pathname)
+      case `file #{pathname.to_s}`
       when /Zip archive data/io
-        ret = `unzip -uo #{file_name}`
+        ret = `unzip -o #{pathname.to_s}`
         ret.lines.select{|v|v.match?(/inflating:/)}.map{|v|v.strip.split(':')[-1].strip}
       else
-        file_name
+        pathname.to_s
       end
     end
 
-    def download(url:, dir:, cleanup: true)
-      raise if url.nil?
+    def downloader(**args)
+      raise UndefinedError
+    end
+
+    def download(url: nil, dir: nil, cleanup: true)
+      raise if dir&.nil?
 
       FileUtils.rm_rf(dir) if cleanup
       FileUtils.mkdir_p(dir)
-      file_name = url.split('/').last
 
       download_files = nil
       Dir.chdir(dir) do
-        uri = URI(url)
-        data = Net::HTTP.get(uri)
-        File.open(file_name, 'w+b'){|f|f.write(data)}
-
-        download_files = after_download(file_name)
+        begin
+          pathname = downloader(url: url, dir: dir)
+        rescue UndefinedError
+          raise if url.nil?
+          pathname = default_downloader(url: url, dir: dir)
+        end
+         download_files = after_download(pathname)
       end
-      if download_files.is_a?(Array)
+
+      case download_files
+      when Array
         download_files.map{|f| File.join(dir, f)}
-      elsif download_files.is_a?(String)
+      when String
         File.join(dir, download_files)
       else
         nil
       end
+    end
+
+    private
+
+    def default_downloader(url:, dir:)
+      file_name = url.split('/').last
+      uri = URI(url)
+      data = Net::HTTP.get(uri)
+      File.open(file_name, 'w+b'){|f|f.write(data)}
+      Pathname.new(file_name)
     end
   end
 end
