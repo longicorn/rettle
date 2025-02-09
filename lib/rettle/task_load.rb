59c62ce8fca71a23fc2b366b64285a0527f7abbf
longicorn <longicorn.c@gmail.com>
2025-02-09 17:31:24 +0900

    feat!: add export on task(load)

--- lib/rettle/task_load.rb
+++ lib/rettle/task_load.rb
@@ -2,5 +2,18 @@
 
 class Rettle
   module TaskLoad
+    def exporter(data)
+      raise UndefinedError
+    end
+
+    def export(data: nil, size: 1000, flush: false)
+      @_export_cache ||= []
+      @_export_cache << data if data
+
+      if flush || @_export_cache.size >= size
+        exporter(@_export_cache) if @_export_cache.size.nonzero?
+        @_export_cache = []
+      end
+    end
   end
 end
